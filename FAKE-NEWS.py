0390dea4c977a0de2637225a766998d5cf988fc6
Akshay Thakur <akshay.181004@gmail.com>
2022-06-19 23:25:04 +0530

    Update FAKE-NEWS.py

--- FAKE-NEWS.py
+++ FAKE-NEWS.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import itertools
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
@@ -35,5 +33,5 @@ y_pred=passiveclassifier.predict(tfidf_test)
 score=accuracy_score(y_test,y_pred)
 
 print(f'Accuracy: {round(score*100,2)}%')
-#DataFlair - Build confusion matrix
+# Build confusion matrix
 print(confusion_matrix(y_test,y_pred, labels=['FAKE','REAL']))
